f3a9a60d1cb08955d8ba97eaa805bf19aa4495d6
chanbroK <koc0819@gmail.com>
2023-05-01 15:55:03 +0900

    chore: add commnet

--- utils/utils.go
+++ utils/utils.go
@@ -11,6 +11,7 @@ const ErrorLogColor = "\033[1;31m%s\033[0m"
 
 var logger = log.New(os.Stderr, "[LOG] ", log.Ldate|log.Ltime)
 
+// handle only generate by using the errors.New fn
 type stackTracer interface {
 	StackTrace() errors.StackTrace
 }
@@ -19,7 +20,7 @@ func ErrorHandler(err error) {
 	logger.Printf(ErrorLogColor, err)
 	if err, ok := errors.Cause(err).(stackTracer); ok {
 		stacks := err.StackTrace()
-		// skip golang runtime error frame(runtime.proc, goexit)
+		// skip runtime error frame(runtime.proc, goexit package)
 		fmt.Printf("%+v", stacks[0:len(stacks)-2])
 	}
 }
